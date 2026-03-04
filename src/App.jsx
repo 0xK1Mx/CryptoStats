8543b7df969e3f1f1a4965b1d5d3beef093255ff
kim <lykimThanh0202@outlook.com>
2026-03-04 16:52:43 -0500

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 
 import "./style.css";
 import Signup from "./pages/Signup";
-import Homepage from "./Pages/Homepage";
+import Homepage from "./pages/Homepage";
 import Dashboard from "./pages/Dashboard";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Header from "./components/Header";
